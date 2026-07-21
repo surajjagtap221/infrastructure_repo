257d3cec835f6aca44b4ac88d9eff8bda990b01a
Surajjagtap221 <srj412305@gmail.com>
2026-07-21 18:47:40 +0530

    complete infrastructure_repo but still there are future improvements as requriments occures + add eks_readonly role

--- terraform_infra/eks/outputs.tf
+++ terraform_infra/eks/outputs.tf
@@ -2,7 +2,7 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.eks_cluster.name
 }
 output "eks_cluster_id" {
- value = aws_eks_cluster.eks_cluster.id
+  value = aws_eks_cluster.eks_cluster.id
 }
 output "eks_cluster_arn" {
   value = aws_eks_cluster.eks_cluster.arn
